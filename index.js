38ed06eba0c0e6e477bdbd3f147918bce39e0ea8
Benjamin_mug <mugangubenjamin696@gmail.com>
2025-03-17 14:38:38 +0200

    Premier commit - Initialisation du projet MERN

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"; 
+
+const app = express(); 
+const port = 4000; 
+
+app.get ("/", (_, res) => {
+    res .send ("Hello Express.......  !");
+    } );   
+
+app.listen (port, () => { 
+    console.log (`Server running on port ${port}`); 
+} );
