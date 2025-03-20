d18a5afd6f36325a83b6cff75b6ec34bacd15e28
Benjamin_mug <mugangubenjamin696@gmail.com>
2025-03-20 23:08:25 +0200

    Mise à jour du projet

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-import express from "express"; 
+import express from "express";
+import dotenv from "dotenv"; // Importation de dotenv pour gérer les variables d'environnement (créer un fichier .env à la racine du projet) et les charger dans process.env (voir https://www.npmjs.com/package/dotenv) 
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const app = express();
+const port = 4000;
 
-const app = express(); 
-const port = 4000; 
+// Connection URI
 
-app.get ("/", (_, res) => {
-    res .send ("Hello Express.......  !");
-    } );   
+dotenv.config(); // Chargement des variables d'environnement dans process.env à partir du fichier .env à la racine du projet (voir https://www.npmjs.com/package/dotenv) 
 
-app.listen (port, () => { 
-    console.log (`Server running on port ${port}`); 
-} );
+const uri = process.env.MONGO_URI; // Récupération de la variable d'environnement STRING_URI (voir https://docs.mongodb.com/drivers/node/connection) 
+// Create a MongoClient with options
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: false,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("✅ Connexion réussie à MongoDB !");
+  } catch (error) {
+    console.error("❌ Erreur de connexion à MongoDB :", error);
+  }
+}
+
+// Appel de la fonction pour se connecter à MongoDB
+connectDB();
+
+app.get("/", (_, res) => {
+  res.send("Hello Express....... !");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+});
